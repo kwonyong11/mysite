9e845fe6bfb95d06d0b2361475d06c7150585ce8
BIT_R <BIT_R@DESKTOP-CTRFTLR>
2020-12-28 18:56:56 +0900

    mysite board추가

---
+++ mysite02/src/main/java/com/bitacademy/mysite/mvc/board/DeleteAction.java
@@ -0,0 +1,24 @@
+package com.bitacademy.mysite.mvc.board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.mysite.repository.BoardRepository;
+import com.bitacademy.web.mvc.Action;
+import com.bitacademy.web.util.WebUtil;
+
+public class DeleteAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardRepository repository = new BoardRepository();
+		Long no = Long.parseLong(request.getParameter("no"));
+		
+		repository.delete(no);
+		WebUtil.redirect(request, response, request.getContextPath()+ "/board" );
+	}
+
+}
